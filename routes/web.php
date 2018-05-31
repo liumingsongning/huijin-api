19ff276812a78002cc57d6e1d9413611159ac7e5
Your Name <you@example.com>
2018-05-31 13:01:17 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::post('/webhook', function () {
     $data = shell_exec('ls -l');
     echo $data;
     $data2=shell_exec("cd {$path} && sudo /usr/bin/git pull ");
-    echo 111111111111111111111111;
+    echo 1111111111111111111111112;
     echo 'zheshi'.$data2;
     // // dd($output);
     // return ['success'=>$data];
