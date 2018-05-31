38c28f0557c96eb5265a2b9cd2f95bf387e2be6f
Your Name <you@example.com>
2018-05-31 14:30:59 +0800

    signe

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,12 @@ Route::post('/webhook', function (Request $request) {
 
     $request_data = file_get_contents('php://input');
 
-    $signe=hash_hmac('sha1', $request_data, 'liumingsongning');
+    $signe=hash_hmac('sha1', $request->payload, 'liumingsongning');
     $path=base_path();
     shell_exec("cd {$path} && sudo /usr/bin/git reset --hard origin/master && sudo /usr/bin/git clean -f && sudo /usr/bin/git pull 2>&1");
     
     // return ['data'=>json_decode($request->payload)->repository->id];
-    return ['data'=>$request_data];
-    
+    return ['data'=>$signe];
+
 
 });
