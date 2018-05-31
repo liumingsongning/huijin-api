b051c6ce363f1144fd0affca127216347ba9339c
Your Name <you@example.com>
2018-05-31 11:52:46 +0800

    echo exec('whoami')

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,10 @@ Route::get('/test', function () {
     dd(9);
 });
 Route::post('/webhook', function () {
-
+    echo exec('whoami') ;
     $path=base_path();
-    shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1",$output);
+    exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1",$output);
+    dd($output);
     return ['success'=>$output];
-    
+
 });
