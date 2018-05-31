7ba4979ec4c50ca4b92de18cca1b270b286702b8
Your Name <you@example.com>
2018-05-31 13:43:15 +0800

    repository

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::post('/webhook', function (Request $request) {
     $path=base_path();
     shell_exec("cd {$path} && sudo /usr/bin/git reset --hard origin/master && sudo /usr/bin/git clean -f && sudo /usr/bin/git pull 2>&1");
     
-    return ['data'=>$request->payload->ref];
+    return ['data'=>json_decode($request->payload)->repository];
     echo 444444;
-    
+
 });
