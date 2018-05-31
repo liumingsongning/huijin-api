f67fb266622c616374956210483f395d1bad7183
Your Name <you@example.com>
2018-05-31 15:13:51 +0800

    test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,12 +12,13 @@ class WebhookController extends Controller
         $request_data = file_get_contents('php://input');
 
         $Signature = hash_hmac('sha1', $request_data, 'liumingsongning');
-
+        echo $Signature;
+        echo $Signature== $request->header('X-Hub-Signature');
         if ($Signature == $request->header('X-Hub-Signature')) {
 
             $path = base_path();
             $id = json_decode($request->payload)->repository->id;
-
+            echo($id);
             switch ($id) {
                 case '135518485':
                     shell_exec("cd {$path} && sudo /usr/bin/git reset --hard origin/master && sudo /usr/bin/git clean -f && sudo /usr/bin/git pull 2>&1");
