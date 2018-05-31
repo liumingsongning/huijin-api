de1d306f4574c599ab3b71c04798875dca2d2c5a
Your Name <you@example.com>
2018-05-31 11:25:47 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Route::post('/webhook', function () {
     $path=base_path();
     shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1",$output);
     return ['success'=>$output];
+    
 });
