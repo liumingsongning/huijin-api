0261de03349a17810dee461b9594d7921bc30ce3
Your Name <you@example.com>
2018-06-04 16:52:44 +0800

    redis

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/test', function () {
     Redis::set('123456', '123', 'EX', 3000);
 });
 Route::get('/test1', function () {
-    Redis::get('123456');
+    return Redis::get('123456');
 });
 
 Route::post('/webhook', 'WebhookController@pull');
