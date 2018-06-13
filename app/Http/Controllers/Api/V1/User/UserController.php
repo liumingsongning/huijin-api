8dd21680865640e74944cba5b111783dfa6b2c69
Your Name <you@example.com>
2018-06-13 16:13:15 +0800

    userinfo

--- app/Http/Controllers/Api/V1/User/UserController.php
+++ app/Http/Controllers/Api/V1/User/UserController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\APi\V1\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\BaseController;
 
-class UserController extends Controller
+class UserController extends BaseController
 {
-    public function userinfo(){
+    public function userinfo(Request $request){
         return $this->response->array(['user'=>$request->user()]);
     }
 }
