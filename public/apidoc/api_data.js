4f3bd669e01789d64a3309cab4e93ceab27b4b7e
Your Name <you@example.com>
2018-06-14 13:43:46 +0800

    apidoc

--- public/apidoc/api_data.js
+++ public/apidoc/api_data.js
@@ -128,6 +128,44 @@ define({ "api": [
     "groupTitle": "Address"
   },
   {
+    "type": "delete",
+    "url": "/address/:id",
+    "title": "address delete",
+    "name": "deleteAddress",
+    "group": "Address",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n  \"success\": \"1\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "AccessDenied",
+            "description": "<p>The phone of the User was error.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 403 Access Denied\n{\n  \"message\": \"删除失败\",\n  \"status_code\": 403,\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "D:/huihjin/git/huijin-api/app/Http/Controllers/Api/v1/Address/AddressController.php",
+    "groupTitle": "Address"
+  },
+  {
     "type": "put",
     "url": "/address/:id",
     "title": "address update",
@@ -631,6 +669,88 @@ define({ "api": [
         {
           "title": "Error-Response:",
           "content": "HTTP/1.1 400 Access Denied\n{\n  \"message\": \"人机验证失败\",\n  \"status_code\": 400,\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "D:/huihjin/git/huijin-api/app/Http/Controllers/Api/v1/Login/LoginController.php",
+    "groupTitle": "Login"
+  },
+  {
+    "type": "post",
+    "url": "/phonebind",
+    "title": "phonebind",
+    "name": "phonebind",
+    "group": "Login",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "phone",
+            "description": "<p>User phone.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "code",
+            "description": "<p>code</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "type",
+            "description": "<p>oauth type.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "oauth_id",
+            "description": "<p>oauth oauth_id</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n  \"token\": \"$token\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "AccessDenied",
+            "description": "<p>The phone of the User was error.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "    HTTP/1.1 403 Access Denied\n    {\n      \"message\": \"验证码不正确\",\n      \"status_code\": 403,\n    }\nor",
+          "type": "json"
+        },
+        {
+          "title": "Error-Response:",
+          "content": "    HTTP/1.1 500 Access Denied\n    {\n      \"message\": \"关联qq用户失败\",\n      \"status_code\": 500,\n    }\nor",
+          "type": "json"
+        },
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 422 Access Denied\n{\n  \"message\": \"未查到该qq用户\",\n  \"status_code\": 422,\n}",
           "type": "json"
         }
       ]
