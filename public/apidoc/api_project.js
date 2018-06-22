66e86718b8ab275a97e7c1c4f806118429d1e50e
Your Name <you@example.com>
2018-06-22 10:13:53 +0800

    doc

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-06-20T03:35:36.322Z",
+    "time": "2018-06-22T02:13:43.123Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
